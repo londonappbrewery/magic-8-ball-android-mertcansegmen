01e1329bbd688ea87479000e052fed7c125782b2
mertcansegmen <mertcan.segmen@hotmail.com>
2018-04-23 02:50:35 +0300

    commit made:D

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final String[] decisions = {"YES!", "NO!"};
+
+        Button askButton = findViewById(R.id.askButton);
+
+        final TextView decisionText = findViewById(R.id.decisionText);
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random rnd = new Random();
+                int randomNumber = rnd.nextInt(2);
+
+                decisionText.setText(decisions[randomNumber]);
+
+            }
+        });
+
+
     }
 }
